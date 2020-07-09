f130b5c7e0714784f493bb510338d9499e671874
Pawel Piecuch <pawel.piecuch@roche.com>
2020-07-09 22:35:54 +0200

    enable transpiler module

--- modules/gdscript_transpiler/config.py
+++ modules/gdscript_transpiler/config.py
@@ -1,11 +1,11 @@
 
 def can_build(env, platform):
-	return True
+    # Editor only module
+    return env['tools']
 
 def configure(env):
 	pass
 
 def is_enabled():
-	# Disabled by default being experimental at the moment.
 	# Enable manually with `module_gdscript_transpiler_enabled=yes` option.
-	return False
+	return True
